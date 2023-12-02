6204887c5ba4af46f5723027ecb9cdf89a72b4f3
Gerardo Urzua <80187497+geUrz@users.noreply.github.com>
2023-12-02 00:12:42 -0800

    Add server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-  SERVER_HOST: 'http://localhost:1337',
-  API_URL: 'http://localhost:1337/api',
+  SERVER_HOST: 'http://lista-canciones-strapi.onrender.com',
+  API_URL: 'http://lista-canciones-strapi.onrender.com/api',
   ENDPOINTS: {
     AUTH: {
       REGISTER: 'auth/local/register',
